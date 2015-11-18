09c6d409761c5a3ad79d1612844991be1c82209c
Grogbart <gries_georg@web.de>
2015-11-18 15:56:17 +0100

    Update input_worker.py

--- input_worker.py
+++ input_worker.py
@@ -2,6 +2,34 @@ from queue_manager import QueueClient
 
 __author__ = 'david'
 
+alexa_splits = [1000000, 1000, 100]
+
+# creates list from AlexaCSV with dated sources
+def datedAlexaCSV(date, splits):
+    splits.sort()
+    m = splits[len(splits) - 1] + 1
+    r = []
+    f = open("top-1m.csv", "r")
+    for i in range(1,m):
+        c = str(i) + ','
+        t = f.readline().replace('\n', '').replace(c, '')
+        q = []
+        for s in splits:
+            q.append(date + "AlexaTOP" + str(s))
+        r.append([t, q])
+        i += 1
+        if i > splits[0]:
+            splits.remove(splits[0])
+    f.close()
+    return r
+
+
+# downloads and unzips AlexaCSV and return the date
+def getAlexaCSV():
+    # TODO: downloading, unzipping and dating
+    return "11_2015"
+
+
 
 if __name__ == "__main__":
 
@@ -11,8 +39,5 @@ if __name__ == "__main__":
     # get appropriate queue from QueueClient
     host_queue = c.host_queue()
 
-    for i in range(20):
-        # put hostname to host_queue
-        host_queue.put("google.com")
-
-
+    d = getAlexaCSV()
+    host_queue.put(datedAlexaCSV(d, alexa_splits))
